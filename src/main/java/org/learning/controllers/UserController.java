275c8d23e48d1bad26b0f70b5ec3fcfa2b002bcc
Aniket Paranjpe <aniket.paranjpe87@gmail.com>
2021-12-19 22:58:17 +0530

    first commit

---
+++ src/main/java/org/learning/controllers/UserController.java
@@ -0,0 +1,17 @@
+package org.learning.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() throws Exception {
+        return ResponseEntity.ok("Users");
+    }
+}
